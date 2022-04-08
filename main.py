86c1e7e05e9efc892d015d4bd28d68f205f4b54c
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2022-04-08 13:57:35 +0200

    AI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import sys
-
+import math
 import numpy as np
 import pygame
 
@@ -29,7 +29,7 @@ def is_valid(board, col):
 
 
 # Get row of insertion
-def current_location(board, col):
+def get_row(board, col):
     row = 0  # point at bottom row
     while board[row][col] != 0:
         row += 1
@@ -118,6 +118,8 @@ def check_move(temp_board, r, c, num, tile=AI):
     open_4 = True  # check if four is possible
     open_right = True  # check if right is open
     open_left = True  # check if left is open
+    open_bottom = True
+    open_top = True
     while temp_col >= 0 and temp_board[temp_row][temp_col] == tile:
         count += 1
         temp_col -= 1
@@ -148,17 +150,25 @@ def check_move(temp_board, r, c, num, tile=AI):
             multiples += 1
     count = 0
     temp_col = c
+    open_4 = True
 
     # Check Vertical
     while temp_row >= 0 and temp_board[temp_row][temp_col] == tile:
         count += 1
         temp_row -= 1
+    # bottom = temp_row
+    # if bottom == -1 or temp_board[bottom][temp_col] == PLAYER:
+    #     open_bottom = False
     temp_row = r
     while temp_row < ROW_COUNT and temp_board[temp_row][temp_col] == tile:
         count += 1
         temp_row += 1
+    top = temp_row
+    if top == ROW_COUNT or temp_board[top][temp_col] == PLAYER:
+        open_4 = False
     if count - 1 == num:
-        multiples += 1
+        if open_4:
+            multiples += 1
     count = 0
 
     # Check Positive Diagonal
@@ -204,9 +214,9 @@ def score_position(board):
     # Horizontal
     for c in range(COL_COUNT):
         if is_valid(copy_board, c):
-            r = current_location(copy_board, c)
+            r = get_row(copy_board, c)
             add_tile(copy_board, r, c, AI)
-            # if c in scores:
+            # Offensive
             scores[c] = check_move(copy_board, r, c, 2) * 2 + scores.get(c, 0)
             scores[c] = check_move(copy_board, r, c, 3) * 4 + scores.get(c, 0)
             if check_win(copy_board, r, c, AI):
@@ -214,6 +224,190 @@ def score_position(board):
                 return c
             remove_tile(copy_board, r, c)
     return max(scores, key=scores.get)
+
+
+def scoring(window):
+    score = 0
+    if window.count(AI) == 4:
+        score += 100
+    elif window.count(AI) == 3 and window.count(0) == 1:
+        score += 5
+    elif window.count(AI) == 2 and window.count(0) == 2:
+        score += 2
+    if window.count(PLAYER) == 3 and window.count(0) == 1:
+        score -= 10
+    return score
+
+
+def get_score(board):
+    score = 0
+    # Center
+    center = [int(i) for i in list(board[:, COL_COUNT//2])]
+    count = center.count(AI)
+    score += count*3
+
+    # Horizontal
+    for r in range(ROW_COUNT):
+        current_row = [int(i) for i in list(board[r, :])]
+        for c in range(COL_COUNT - 3):
+            window = current_row[c:c + 4]
+            score += scoring(window)
+
+    # Vertical
+    for c in range(COL_COUNT):
+        current_col = [int(i) for i in list(board[:, c])]
+        for r in range(ROW_COUNT - 3):
+            window = current_col[r:r + 4]
+            score += scoring(window)
+
+    # Positive Diagonal
+    for r in range(ROW_COUNT-3):
+        for c in range(COL_COUNT-3):
+            window = [board[r+i][c+i] for i in range(4)]
+            score += scoring(window)
+
+    # Negative Diagonal
+    for r in range(ROW_COUNT - 3):
+        for c in range(COL_COUNT - 3):
+            window = [board[r + 3 - i][c + i] for i in range(4)]
+            score += scoring(window)
+
+    return score
+
+
+def pick_best_move(board):
+    valid = valid_locations(board)
+    best = 0
+    best_col = random.choice(valid)
+    for c in valid:
+        r = get_row(board, c)
+        temp_board = board.copy()
+        add_tile(temp_board, r, c, AI)
+        score = get_score(temp_board)
+        if score > best:
+            best = score
+            best_col = c
+    return best_col
+
+
+def winning_move(board, tile):
+    # Check horizontal locations for win
+    for c in range(COL_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == tile and board[r][c + 1] == tile and board[r][c + 2] == tile and board[r][c + 3] == tile:
+                return True
+
+    # Check vertical locations for win
+    for c in range(COL_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == tile and board[r + 1][c] == tile and board[r + 2][c] == tile and board[r + 3][c] == tile:
+                return True
+
+    # Check positively sloped diaganols
+    for c in range(COL_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == tile and board[r + 1][c + 1] == tile and board[r + 2][c + 2] == tile and board[r + 3][c + 3] == tile:
+                return True
+
+    # Check negatively sloped diaganols
+    for c in range(COL_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == tile and board[r - 1][c + 1] == tile and board[r - 2][c + 2] == tile and board[r - 3][c + 3] == tile:
+                return True
+
+
+def is_terminal(board):
+    return winning_move(board, AI) or winning_move(board, PLAYER) or len(valid_locations(board)) == 0
+
+
+def minimax(board, depth, maximizing_player):
+    valid = valid_locations(board)
+    if depth == 0 or is_terminal(board):
+        if is_terminal(board):
+            if winning_move(board, AI):
+                return None, 1000000
+            elif winning_move(board, PLAYER):
+                return None, -100000
+            else:
+                return None, 0
+        else:
+            return None, get_score(board)
+    if maximizing_player:
+        value = -math.inf
+        best_col = random.choice(valid)
+        for c in valid:
+            r = get_row(board, c)
+            temp_board = board.copy()
+            add_tile(temp_board, r, c, AI)
+            score = minimax(temp_board, depth-1, False)[1]
+            if score > value:
+                value = score
+                best_col = c
+        return value, best_col
+    else:
+        value = math.inf
+        best_col = random.choice(valid)
+        for c in valid:
+            r = get_row(board, c)
+            temp_board = board.copy()
+            add_tile(temp_board, r, c, PLAYER)
+            score = minimax(temp_board, depth - 1, True)[1]
+            if score < value:
+                value = score
+                best_col = c
+        return value, best_col
+
+
+def minimax_alpha_beta(board, depth, alpha, beta, maximizing_player):
+    valid = valid_locations(board)
+    if depth == 0 or is_terminal(board):
+        if is_terminal(board):
+            if winning_move(board, AI):
+                return None, 1000000
+            elif winning_move(board, PLAYER):
+                return None, -100000
+            else:
+                return None, 0
+        else:
+            return None, get_score(board)
+    if maximizing_player:
+        value = -math.inf
+        best_col = random.choice(valid)
+        for c in valid:
+            r = get_row(board, c)
+            temp_board = board.copy()
+            add_tile(temp_board, r, c, AI)
+            score = minimax_alpha_beta(temp_board, depth-1, alpha, beta, False)[1]
+            if score > value:
+                value = score
+                best_col = c
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return value, best_col
+    else:
+        value = math.inf
+        best_col = random.choice(valid)
+        for c in valid:
+            r = get_row(board, c)
+            temp_board = board.copy()
+            add_tile(temp_board, r, c, PLAYER)
+            score = minimax_alpha_beta(temp_board, depth - 1, alpha, beta, True)[1]
+            if score < value:
+                value = score
+                best_col = c
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return value, best_col
+
+
+def valid_locations(board):
+    valid = []
+    for c in range(COL_COUNT):
+        if is_valid(board, c):
+            valid.append(c)
+    return valid
 
 
 def draw_board(board):
@@ -268,7 +462,7 @@ while not game_over:
             if turn == PLAYER:
                 col = event.pos[0] // SQUARESIZE
                 if is_valid(board, col):
-                    row = current_location(board, col)
+                    row = get_row(board, col)
                     add_tile(board, row, col, turn)
                     draw_board(board)
                     if check_win(board, row, col, turn):
@@ -279,10 +473,12 @@ while not game_over:
                     turn = AI
     if turn == AI and not game_over:
         # col = random.randint(0, COL_COUNT-1)
-        col = score_position(board)
+        # col = pick_best_move(board)
+        score, col = minimax(board, 3, True)
+        # score, col = minimax_alpha_beta(board, 3,-math.inf, math.inf, True)
         if is_valid(board, col):
-            pygame.time.wait(500)
-            row = current_location(board, col)
+            pygame.time.wait(100)
+            row = get_row(board, col)
             add_tile(board, row, col, turn)
             draw_board(board)
             if check_win(board, row, col, turn):
@@ -291,6 +487,6 @@ while not game_over:
                 pygame.display.update()
                 game_over = True
             turn = PLAYER
-    print(board)
+    # print(board)
     if game_over:
         pygame.time.wait(3000)
